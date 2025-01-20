c7ba7751ea1563037f1c2f4f32ebb7c2e9f4f23f
Paul Sharpe <paulsharpe577@gmail.com>
2025-01-20 16:47:22 +0000

    Updates for formatting and house logic

--- pages/models.py
+++ pages/models.py
@@ -4,6 +4,7 @@ class House(models.Model):
     address = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    rooms = models.DecimalField(max_digits=10, decimal_places=0)
 
     def __str__(self):
         return self.address
--- pages/views.py
+++ pages/views.py
@@ -4,11 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'pages/home.html')
 
-
 from .models import House
 
 def house_list(request):
     houses = House.objects.all()
-    return render(request, 'house_list.html', {'houses': houses})
+    return render(request, 'pages/house_list.html', {'houses': houses})
 
 
